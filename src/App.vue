2e51cf030210c4d100d33befb18f2624005ab9ab
ByungjooPark <milkpbj23@gmail.com>
2026-04-20 15:46:41 +0900

    260420 [이론] props ~ lifeCycle com

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,13 @@ import EventHandling from './components/EventHandling.vue';
 import ConditionalRendering from './components/ConditionalRendering.vue';
 import ListRandering from './components/ListRandering.vue';
 import Watchers from './components/Watchers.vue';
+import Props from './components/Props.vue';
+import Slot from './components/Slot.vue';
+import MyTransition from './components/MyTransition.vue';
+import { ref } from 'vue';
+import LifeCycle from './components/LifeCycle.vue';
+
+const flgLifeCycle = ref(true);
 </script>
 
 <template>
@@ -13,7 +20,12 @@ import Watchers from './components/Watchers.vue';
   <EventHandling v-if="false" />
   <ConditionalRendering v-if="false" />
   <ListRandering v-if="false" />
-  <Watchers />
+  <Watchers v-if="false" />
+  <Props v-if="false" />
+  <Slot v-if="false" />
+  <MyTransition v-if="false" />
+  <LifeCycle v-if="flgLifeCycle"/>
+  <button type="button" @click="flgLifeCycle = !flgLifeCycle">라이프사이클 컴포넌트 토글</button>
 </template>
 
 <style>
